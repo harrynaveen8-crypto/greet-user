e42b26f514efc9c90cb5e49ebc78d3b09233c78a
Naveen Harry <harry@ip-172-31-23-234.eu-north-1.compute.internal>
2026-05-29 22:27:29 +0530

    Add age verification to greeting script

--- greet.py
+++ greet.py
@@ -1,2 +1,8 @@
 name = input("Enter your name: ")
 print("hi " + name + ", nice to meet you")
+age = int(input("Enter your age: "))
+
+if age > 18:
+    print("here's your beer")
+else:
+    print("no beer for you")
